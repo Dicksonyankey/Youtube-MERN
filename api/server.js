9dd228dbaa54610b22de98762bad5778f9b82cb9
dicksonyankey <yankeydickson1@gmail.com>
2023-11-06 02:45:17 +0000

    Connected to the Mongodb database

--- api/server.js
+++ api/server.js
@@ -1,10 +1,20 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		console.log("Connected to MongoDB Successfully");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+const app = express();
 
-const PORT = 3000
-
+const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT }`);
-})
+	console.log(`Server running on port ${PORT}`);
+});
