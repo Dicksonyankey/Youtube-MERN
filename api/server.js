bc676294533d74ea39616582c3f8ca7363efa3e9
dicksonyankey <yankeydickson1@gmail.com>
2023-11-08 10:51:31 +0000

    Created the signup api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -12,9 +14,13 @@ mongoose
 		console.log(err);
 	});
 const app = express();
+app.use(express.json());
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
